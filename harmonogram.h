c4f9e33c04f48aca47f2861dd6d796bd7b75463e
Hmarkie <56601802+Hmarkie@users.noreply.github.com>
2020-01-21 21:12:30 +0100

    Update harmonogram.h

--- harmonogram.h
+++ harmonogram.h
@@ -4,9 +4,11 @@ using namespace std;
 class harmonogram
 {private:
 	string numer_licencji;
+	int terminy_zajete;
 
 public:
 	harmonogram();
 	~harmonogram();
 	void przeslij_data(string data);
+	int harmonogram_pracy();  //do testów
 };
