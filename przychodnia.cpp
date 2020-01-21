002a1ac346f7c21f50e7e35bda5cfd298473828a
Mariola98 <60109424+Mariola98@users.noreply.github.com>
2020-01-21 20:48:14 +0100

    Update przychodnia.cpp

--- przychodnia.cpp
+++ przychodnia.cpp
@@ -1,12 +1,30 @@
+
+
 #include "pch.h"
 #include "przychodnia.h"
 
 
 przychodnia::przychodnia()
 {
+	numer_karty = 000;
 }
 
 
+przychodnia::~przychodnia()
+{
+
+}
+
+string przychodnia::nadaj_numer_karty_pacjenta(string pesel)
+{
+	numer_karty = 001;
+	return pesel;
+}
+
+int przychodnia::wyswietl_numer()
+{
+	return numer_karty;
+}
 przychodnia::~przychodnia()
 {
 }
