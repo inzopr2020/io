8e3934780188ad3cdebc59e263770bcd15a41e1a
Mariola98 <60109424+Mariola98@users.noreply.github.com>
2020-01-21 20:49:02 +0100

    Update przychodnia.h

--- przychodnia.h
+++ przychodnia.h
@@ -5,9 +5,10 @@ class przychodnia
 {private:
 	string nazwa;
 	string adres;
+	int numer_karty;
 public:
 	przychodnia();
 	~przychodnia();
 	string nadaj_numer_karty_pacjenta(string pesel);
+	int wyswietl_numer();
 };
-
