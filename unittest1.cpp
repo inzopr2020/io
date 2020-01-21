73e3a6c9de0b1f6bc8d103c8761387eeed6b3970
Mariola98 <60109424+Mariola98@users.noreply.github.com>
2020-01-21 13:24:17 +0100

    Update unittest1.cpp

--- unittest1.cpp
+++ unittest1.cpp
@@ -1,6 +1,8 @@
+
 #include "stdafx.h"
 #include "CppUnitTest.h"
-
+#include "../ConsoleApplication2/pacjent_.h"
+#include "../ConsoleApplication2/pacjent_.cpp"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace Testjednostkowy1
@@ -11,9 +13,22 @@ namespace Testjednostkowy1
 		
 		TEST_METHOD(TestMethod1)
 		{
-			// TODO: W tym miejscu dodaj kod testowy
+			pacjent_ p1;
+			Assert::AreEqual(p1.zwroc_il_rezerwacji(), 0);
 
 		}
+		TEST_METHOD(TestMethod2)
+		{
+			pacjent_ p1;
+			p1.zarezerwuj_wizyte("56546", "ucdghu", "gcuhdgy");
+			Assert::AreEqual(p1.zwroc_il_rezerwacji(), 1);
+		}
 
+		TEST_METHOD(TestMethod3)
+		{
+			pacjent_ p1;
+			p1.zarejestruj_pacjenta("Karol", "Kownacki", "Lonika 137", "674526987", "256894762");
+			Assert::AreEqual(p1.numer_pacjenta(), 001);
+		}
 	};
 }
