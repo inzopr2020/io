8ef582854439bd12d208a8f351276e05ea15d33e
Mariola98 <60109424+Mariola98@users.noreply.github.com>
2020-01-21 11:21:44 +0100

    Add files via upload

--- pacjent_.cpp
+++ pacjent_.cpp
@@ -2,40 +2,12 @@
 #include "pacjent_.h"
 
 
-
 pacjent_::pacjent_()
 {
-
 }
+
+
 
 pacjent_::~pacjent_()
 {
-}
-
-void pacjent_::zarezerwuj_wizyte(string data, string specjalizacja, string lekarz)
-{
-	il_rezerwacji++;
-	cout << "Zarezerwowano wizyte u lekarza " << specjalizacja << lekarz << endl;
-}
-
-void pacjent_::zmien_dane(string telefon1, string adres1)
-{
-	telefon = telefon1;
-	adres = adres1;
-
-}
-void pacjent_::zarejestruj_pacjenta(string imie1, string nazwisko1, string adres1, string telefon1, string pesel1, int numer_karty_pacjenta1) 
-{
-	imie = imie1;
-	nazwisko = nazwisko1;
-	telefon = telefon1;
-	il_rezerwacji = 0;
-	adres = adres1;
-	pesel = pesel1;
-	numer_karty_pacjenta = numer_karty_pacjenta1;
-}
-	
-void pacjent_::zwroc_il_rezerwacji()
-{
-	cout << "Ilosc rezerwacji wynosi:" << il_rezerwacji << endl;
 }
--- pacjent_.h
+++ pacjent_.h
@@ -8,18 +8,12 @@ private:
 	string pesel;
 	int numer_karty_pacjenta;
 	int il_rezerwacji;
-	string telefon;
-	string adres;
-	string imie;
-	string nazwisko;
-
 public:
 	void zarezerwuj_wizyte(string data, string specjalizacja, string lekarz);
-	void zmien_dane(string telefon1, string adres1);
-	void zarejestruj_pacjenta(string imie1, string nazwisko1, string adres1, string telefon1, string pesel1, int numer_karty_pacjenta1);
+	void zmien_dane(string telefon, string adres);
+	void zarejestruj_pacjenta(string imie, string nazwisko, string adres, string telefon, string pesel);
 	void zwroc_il_rezerwacji();
 	pacjent_();
 	virtual ~pacjent_();
-	
 };
 
